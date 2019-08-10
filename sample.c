31ee804293a97da84050253744a1a8e644db1846
Potnuri-Sai-Teja <45753262+Potnuri-Sai-Teja@users.noreply.github.com>
2019-08-10 16:12:44 +0530

    Code.c

--- sample.c
+++ sample.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 void main()
 {
-printf("Welcome to Github");
+int i,j;
+int sum=0;
+scanf("%d%d",&i,&j);
+While(i!=1)
+{
+   i=j/i×100;
+  if(i!=0 || i!=1){
+scanf("%d%d",&i,&j);
+}
+   sum=sum+i;
+}
+Printr("%d \n",sum);
 }
